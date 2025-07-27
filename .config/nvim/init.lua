fc5301f8ee0b01a236512be11ac76e2002c24067
Georgi Kakov <gmkukov@gmail.com>
2025-07-27 20:50:30 +0300

    Reorg keymap options

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,2 @@
---vim.g.mapleader = " "
---
----- open terminal in bottom right
---vim.api.nvim_set_keymap('n', '<Leader>t', ':10 split | term<CR>', { noremap = true, silent = true })
---
 require('gyogy')
 
--- .config/nvim/lua/gyogy/keymaps.lua
+++ .config/nvim/lua/gyogy/keymaps.lua
@@ -1,4 +1,6 @@
+vim.g.mapleader = " "
 
 -- Keybindings for common actions
 vim.api.nvim_set_keymap('n', '<Leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>o', ':lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Leader>t', ':10 split | term<CR>', { noremap = true, silent = true })
