9c82e129a074e7f22121f78e68a57d4d7e0280a1
Georgi Kakov <gmkukov@gmail.com>
2025-10-17 16:37:49 +0300

    Added line softwrapping for plain text. Not running it on save, because of formatter time outs

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -6,13 +6,33 @@ return {
             formatters_by_ft = {
                 python = { "isort", "black" },
                 json = { "jq" },
+                text = { "softwrap_plain_text" },
+                [""] = { "softwrap_plain_text" },
                 ["*"] = { "trim_whitespace" },
             },
-            format_on_save = {
-                timeout_ms = 500,
-                lsp_format = "fallback",
+
+        format_on_save = function(bufnr)
+            local ft = vim.bo[bufnr].filetype
+            if ft == "text" or ft == "" then
+                return false -- skip for plain text
+            end
+            return { timeout_ms = 2000, lsp_format = "fallback" }
+        end,
+
+
+        -- Softwrap lines longer than textwidth in plain text
+        formatters = {
+            softwrap_plain_text = {
+                format = function(_, ctx)
+                    vim.api.nvim_buf_call(ctx.bufnr or 0, function()
+                        vim.cmd("silent keepjumps keeppatterns %normal! gw$")
+                    end)
+                    return { text = nil }
+                end,
             },
-        })
+        },
+    })
+
 
         -- <leader>f: format buffer / visual range; exit visual mode after formatting
         vim.keymap.set("", "<leader>f", function()
